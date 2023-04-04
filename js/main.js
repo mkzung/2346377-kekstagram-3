fd97a632274efec8c757223ba4dfcae68372d937
mkzung <103102868+mkzung@users.noreply.github.com>
2023-04-04 16:18:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function getRandomInt(min, max) {
   return Math.floor(min + Math.random() * (max - min));
 }
 
+/*
 function checkCommentLength(string, requiredLength) {
   if(string.length <= requiredLength) {
     return true;
@@ -9,9 +10,30 @@ function checkCommentLength(string, requiredLength) {
     return false;
   }
 }
+*/
 
 const button = document.querySelector('.img-upload__label');
 button.onclick = function() {
   getRandomInt(1, 100);
   checkCommentLength('sdgs', 3);
+  generateContent(25);
 };
+
+function generateContent(size) {
+  const content = [];
+  for(let i = 0; i < size - 1; i++) {
+    content[i] = generateObj(i + 1);
+  }
+  return content;
+}
+
+function generateObj(id) {
+  const newObj = {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: '',
+    likes: getRandomInt(15, 200),
+    comments: getRandomInt(0, 200)
+  };
+  return newObj;
+}
