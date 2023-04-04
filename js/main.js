e7308d50b247a05c087994412ecb66fcd4406f9b
mkzung <103102868+mkzung@users.noreply.github.com>
2023-04-04 16:44:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { generateContent } from './data.js';
 import { drawPictures } from './draw.js';
 import './form.js';
 import './validate.js';
+import './addEffect.js';
 
 
 const content = generateContent(25);
